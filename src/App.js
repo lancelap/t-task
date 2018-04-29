da2505ec79f7d3b1ca6063259e0a1e972ffcb79f
lancelap <lancelap@lancelaps-iMac.local>
2018-04-29 13:25:45 +0500

    initial config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import { Router } from 'react-router-dom';
+import store from './redux';
+import Root from './components/Root';
+import history from './history';
 
 class App extends Component {
   render() {
     return (
-      <div>Hello world</div>
-    );
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Root/>
+        </ConnectedRouter>
+      </Provider>
+    )
   }
 }
 
---
+++ src/components/Root.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Root extends Component {
+  render() {
+    return(
+        <h1>Hello</h1>
+    )
+  }
+}
+
+export default Root;
